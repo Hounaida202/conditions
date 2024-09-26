9d7c0b9266439627e789ab1a0ac451f38ff36e93
Hounaida202 <etrichih@gmail.com>
2024-09-26 12:55:03 +0100

    ajout un autre

---
+++ bchallenge6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+    int main(){
+    int n;
+    printf("donner un nombre");
+    scanf("%d",&n);
+
+    if(n>0){
+
+
+    printf("le nombre est positif");
+
+
+ }else{
+    if(n<0){
+    printf("le nombre est negatif");
+
+    }
+
+    else
+   { printf("le nombre est nul");}
+
+
+    }
+
+    }
